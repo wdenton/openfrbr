fd9dc28de760617a2a1730c5d504adeaef1e37a0
William Denton <wtd@pobox.com>
2008-08-02 11:37:40 -0400

    Some CSS Added default route so that hitting / now lists works; removed index.html

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,6 @@
 class Work < ActiveRecord::Base
 
+  
   has_many :expressions
   has_and_belongs_to_many :people, :join_table => "works_people"
   has_and_belongs_to_many :corporate_bodies, :join_table => "works_corporate_bodies"
