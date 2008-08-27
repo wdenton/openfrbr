c83ce5d0e049fa2276dae55e7cdbb1a32bcafa07
William Denton <wtd@pobox.com>
2008-08-26 22:50:49 -0400

    List subjects on work pages with subject_name helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
 
+  def subject_name(subject)
+    # The attribute we use to refer to a subject depends on
+    # what kind of entity it is.  
+    if %w[Work Expression Manifestation Item].include?(subject.class.to_s)
+      return subject.title
+    elsif %w[Person Family CorporateBody].include?(subject.class.to_s)
+      return subject.name
+    elsif %w[Concept Event FrbrObject Place].include?(subject.class.to_s)
+      return subject.term
+    end
+  end
+
 end
