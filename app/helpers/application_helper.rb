fe49f5bbdefb0e11f69409e5b86a2c03f5cf1f15
William Denton <wtd@pobox.com>
2010-01-29 14:43:00 -0500

    Change how Expression and Work appear on Manifestation page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
 
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
+
 end
