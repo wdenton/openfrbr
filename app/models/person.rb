59200af0659eb011780dd8b049e017428567f1a0
William Denton (eee) <wtd@pobox.com>
2010-02-23 00:25:27 -0500

    Show 'Subject of' for People, from their Aboutnesses

--- app/models/person.rb
+++ app/models/person.rb
@@ -10,7 +10,7 @@ class Person < ActiveRecord::Base
   has_many :works,          :as => :subject
 
   def anchor_text
-    name + " (" + creations.size.to_s + " w)"
+    name # + " (" + creations.size.to_s + " w)"
   end
 
 end
