5010b4b485785a2a3a89d040f17566236c4ac904
William Denton <wtd@pobox.com>
2010-05-27 12:59:27 -0400

    Fiddly fiddly

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-#
-
 set :application, "openfrbr"
 set :repository,  "git://github.com/wdenton/openfrbr.git"
 set :deploy_to, "/var/www/openfrbr.org"
@@ -9,7 +7,7 @@ server "anvil.lisforge.net", :app, :web, :db, :primary => true
 
 set :user, "wtd"
 set :runner, "wtd"
-
+set :use_sudo, false
 
 namespace :deploy do
   task :start, :roles => :app do
@@ -25,24 +23,3 @@ namespace :deploy do
     run "touch #{current_release}/tmp/restart.txt"
   end
 end
-
-# namespace :deploy do
-#   desc "Restarting mod_rails with restart.txt"
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "touch #{current_path}/tmp/restart.txt"
-#   end
-
-#   [:start, :stop].each do |t|
-#     desc "#{t} task is a no-op with mod_rails"
-#     task t, :roles => :app do ; end
-#   end
-# end
-
-#namespace :passenger do
-#  desc "Restart Application"
-#  task :restart do
-#    run "touch #{current_path}/tmp/restart.txt"
-#  end
-#end
-
-#after :deploy, "passenger:restart"
